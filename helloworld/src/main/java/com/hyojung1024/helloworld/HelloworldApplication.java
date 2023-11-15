a34fae7ae7c534d45d1c476a8d350f53162fc1a3
hyojung1024 <greeni1024@naver>
2023-11-15 11:47:17 +0900

    HelloWorld Project Add

---
+++ helloworld/src/main/java/com/hyojung1024/helloworld/HelloworldApplication.java
@@ -0,0 +1,21 @@
+package com.hyojung1024.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class HelloworldApplication {
+
+	@GetMapping(value="/")
+	public String index(){
+		return "Hello World with Spring Boot";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldApplication.class, args);
+	}
+
+}
---
+++ helloworld/src/test/java/com/hyojung1024/helloworld/HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyojung1024.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
